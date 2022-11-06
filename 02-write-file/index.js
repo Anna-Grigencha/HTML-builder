c40b3850a5c3556946e00fef56024fc29e071481
Anna-Grigencha <grigencha10@gmail.com>
2022-11-06 17:37:17 +0300

    feat: add 01-read-file and 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');//создание потока записи в текстовый файл
+const { stdout } = process; 
+const { stdin } = process;
+stdout.write('Введите имя участника:\n');//Выводит строку в консоль
+stdin.on('data', chunk => 
+{
+  if (chunk.toString().trim() === 'exit'){
+    process.exit(); 
+  }
+  output.write(chunk);
+  
+});
+process.on('exit', () => stdout.write('\nХорошего дня!'));
+process.on('SIGINT', () => process.exit());  //сигнал SIGINT генерируется при нажатии ctrl+c
