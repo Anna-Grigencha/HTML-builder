651be113e3d60b943018efe2ac6984e4b7eca8c9
Anna-Grigencha <grigencha10@gmail.com>
2023-05-07 16:12:06 +0300

    feat: add commits in tasks 01, 02, 03

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,16 +1,26 @@
+//1. Импорт всех требуемых модулей.
+const { stdin, stdout } = process;
 const path = require('path');
 const fs = require('fs');
-const { stdout } = process; 
-const { stdin } = process;
+
+
+//2. Создание потока записи в текстовый файл
+
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');//создание потока записи в текстовый файл
-stdout.write('Введите имя участника:\n');//Выводит строку в консоль
-stdin.on('data', chunk => 
-{
-  if (chunk.toString().trim() === 'exit'){
-    process.exit(); 
+
+//3. Вывод в консоль приветственного сообщения
+stdout.write('Введите имена участников\n');
+
+//4. Ожидание ввода текста пользователем, с дальнейшей проверкой ввода на наличие ключевого слова **exit**
+//5. Запись текста в файл
+stdin.on('data', chunk => {
+  if (chunk.toString().trim() === 'exit') {
+    process.exit();
   }
   output.write(chunk);
-  
+
 });
+//6. Ожидание дальнейшего ввода
+//7. Реализация прощального сообщения при остановке процесса
 process.on('exit', () => stdout.write('\nХорошего дня!'));
 process.on('SIGINT', () => process.exit());  //сигнал SIGINT генерируется при нажатии ctrl+c
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,20 @@
+//Импорт всех требуемых модулей
 const fs = require('node:fs/promises');
 const path = require('path');
 
 (async () => {
-try {
-    const files = await fs.readdir(path.join(__dirname, './secret-folder'), {
-        withFileTypes: true
-      });
-    for (const file of files) {
-      if (file.isFile()) {
+  try {
+
+    const files = await fs.readdir(path.join(__dirname, './secret-folder'), { //Чтение содержимого папки secret - folder
+      withFileTypes: true
+    });
+    for (const file of files) {     //Получение данных о каждом объекте который содержит папка secret - folder
+      if (file.isFile()) {         //Проверка объекта на то, что он является файлом
         const filePath = path.join(__dirname, './secret-folder', file.name);
         const fileName = path.parse(filePath).name;
         const fileExt = path.extname(filePath).slice(1);
         const fileSize = (await fs.stat(filePath)).size;
-        console.log(`${fileName} - ${fileExt} - ${fileSize}byte`);
+        console.log(`${fileName} - ${fileExt} - ${fileSize}byte`);      //Вывод данных о файле в консоль
       }
     }
   } catch (err) {
